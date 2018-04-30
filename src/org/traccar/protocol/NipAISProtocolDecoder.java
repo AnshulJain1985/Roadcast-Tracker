933a74ee6e896ad1a51e5b2731a7cdb804a843a9
Anshul Jain <anshuljain1985@gmail.com>
2018-04-30 12:19:06 +0530

    new ais nip protocol.

---
+++ src/org/traccar/protocol/NipAISProtocolDecoder.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright 2012 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import org.jboss.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.DeviceSession;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.CellTower;
+import org.traccar.model.Network;
+import org.traccar.model.Position;
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class NipAISProtocolDecoder extends BaseProtocolDecoder {
+
+    public NipAISProtocolDecoder(NipAISProtocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("\n$,N,NIPPON,")
+            .number("(d.d+),")                      // version
+            .expression("([A-Z]+),")                // Packet Type
+            .number("(dd),")                        // Alert ID
+            .expression("([HL]),")                    // Packet Status
+            .expression("([0-9]+),")                // IMEI
+            .expression("([^,]+)?,")                // vehicle reg no
+            .number("(d+),")                        // GPS Fix
+            .number("(dd)(dd)(dddd),")                // date utc (DDMMYYYY)
+            .number("(dd)(dd)(dd),")                // time utc (hhmmss)
+            .number("(-?d+.d+),")                   // latitude
+            .expression("([NS]),")
+            .number("(-?d+.d+),")                   // longitude
+            .expression("([EW]),")
+            .number("(ddd.d),")                     // speed
+            .number("(ddd.dd),")                    // course
+            .number("(d+),")                        // No of satellites
+            .number("(d+.?d*)?,?")                  // altitude
+            .number("(ddd.d),")                     // pdop
+            .number("(ddd.d),")                     // hdop
+            .expression("([^,]+)?,")                // Operator Name
+            .number("(d),")                         // Ignition
+            .number("(d),")                         // Main power status
+            .number("(d+.?d*)?,?")                  // Main input voltage
+            .number("(d+.?d*)?,?")                  // internal battery voltage
+            .number("(d),")                         // Emergency Status
+            .expression("([OC]),")                  // Temper alert
+            .number("(d+),")                        // GSM signal strength
+            .expression("([^,]+)?,")                // MCC
+            .expression("([^,]+)?,")                // MNC
+            .expression("([^,]+)?,")                // LAC
+            .expression("([^,]+)?,")                // CELLID
+            .expression("([^,]+)?,")                // NMR1 CellID
+            .expression("([^,]+)?,")                // NMR1 LAC
+            .number("(d+),")                        // NMR1 signal strength
+            .expression("([^,]+)?,")                // NMR2 CellID
+            .expression("([^,]+)?,")                // NMR2 LAC
+            .number("(d+),")                        // NMR2 signal strength
+            .expression("([^,]+)?,")                // NMR3 CellID
+            .expression("([^,]+)?,")                // NMR3 LAC
+            .number("(d+),")                        // NMR3 signal strength
+            .expression("([^,]+)?,")                // NMR4 CellID
+            .expression("([^,]+)?,")                // NMR4 LAC
+            .number("(d+),")                        // NMR4 signal strength
+            .number("(d)(d)(d)(d),")                // digital Input 4
+            .number("(d)(d),")                      // digital Output 2
+            .number("(d+),")                        // Frame number
+            .number("(xx),")                        // checksum
+            .text("*")
+            .compile();
+
+
+    private String decodeAlarm(String value) {
+
+//        NR: Normal periodic packet
+//        HP: Health packet
+//        TA: Tamper alert
+//        EA: Emergency alert
+//        IN: Ignition On alert
+//        IF: Ignition OFF alert
+//        BR: Mains reconnected alert
+//        BD: Mains disconnected alert
+//        BL: Low battery alert
+//        BH: Low battery charged alert
+//        CC: Configuration over the air alert
+//        HA: Harsh acceleration alert
+//        HB: Harsh braking alert
+//        RT: Harsh/Rash turning alert
+//        OS: Over Speed Alert
+
+        switch (value) {
+            case "NR":
+            case "HP":
+            case "IN":
+            case "IF":
+            case "BH":
+            case "CC":
+                return null;
+            case "TA":
+                return Position.ALARM_TAMPERING;
+            case "EA":
+                return Position.ALARM_SOS;
+            case "BR":
+                return Position.ALARM_POWER_RESTORED;
+            case "BD":
+                return Position.ALARM_POWER_CUT;
+            case "BL":
+                return Position.ALARM_LOW_BATTERY;
+            case "HA":
+                return Position.ALARM_ACCELERATION;
+            case "HB":
+                return Position.ALARM_BRAKING;
+            case "RT":
+                return Position.ALARM_CORNERING;
+            case "OS":
+                return Position.ALARM_OVERSPEED;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String sentence = (String) msg;
+
+        Position position = new Position(getProtocolName());
+
+        Parser parser = new Parser(PATTERN, sentence);
+        if (!parser.matches()) {
+            return null;
+        }
+
+        position.set(Position.KEY_VERSION_FW, parser.next());
+
+        String packetType = parser.next();
+        int alertId = parser.nextInt(0);
+        String packetStatus = parser.next();
+        String imei = parser.next();
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        position.set(Position.KEY_ALARM, decodeAlarm(packetType));
+
+        position.set(Position.KEY_ORIGINAL, parser.next());
+        position.setValid(parser.nextInt(0) == 1);
+
+        DateBuilder dateBuilder = new DateBuilder()
+                .setDateReverse(parser.nextInt(0), parser.nextInt(0), parser.nextInt(0));
+        dateBuilder.setTime(parser.nextInt(0), parser.nextInt(0), parser.nextInt(0));
+
+        position.setTime(dateBuilder.getDate());
+        position.setLatitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_HEM));
+        position.setLongitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_HEM));
+        position.setSpeed(parser.nextDouble(0));
+        position.setCourse(parser.nextDouble(0));
+        position.set(Position.KEY_SATELLITES, parser.nextInt(0));
+        position.setAltitude(parser.nextDouble(0));
+        position.set(Position.KEY_PDOP, parser.nextDouble(0));
+        position.set(Position.KEY_HDOP, parser.nextDouble(0));
+        position.set(Position.KEY_OPERATOR, parser.next());
+        position.set(Position.KEY_IGNITION, parser.nextInt(0) == 1);
+        position.set(Position.KEY_CHARGE, parser.nextInt(0) == 1);
+        position.set(Position.KEY_POWER, parser.nextDouble(0));
+        position.set(Position.KEY_BATTERY, parser.nextDouble(0));
+        position.set(Position.KEY_STATUS, parser.nextInt(0));
+
+        String temperAlert = parser.next();
+
+        position.set(Position.KEY_RSSI, parser.nextInt(0));
+
+
+        Network network = new Network();
+
+        int mcc = parser.nextHexInt();
+        int mnc = parser.nextHexInt();
+        int lac = parser.nextHexInt();
+        int cellId = parser.nextHexInt();
+
+        for (int i = 0; i < 4; i++) {
+            int cellIdN = parser.nextHexInt();
+            int lacN = parser.nextHexInt();
+            int rssiN = parser.nextHexInt();
+            network.addCellTower(CellTower.from(mcc, mnc, lacN, cellIdN, rssiN));
+        }
+
+        for (int i = 1; i <= 4; i++) {
+            position.set(Position.PREFIX_IN + i, parser.nextInt(0));
+            if (i == 2) {
+                position.set(Position.KEY_DOOR, parser.nextInt(0) == 1);
+            }
+        }
+        for (int i = 1; i <= 2; i++) {
+            position.set(Position.PREFIX_OUT + i, parser.nextInt(0));
+        }
+
+        int frameNumber = parser.nextInt(0);
+        String checksum = parser.next();
+
+        return position;
+    }
+
+}
