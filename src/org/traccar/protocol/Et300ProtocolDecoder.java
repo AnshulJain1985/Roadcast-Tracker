eba7e108670d4debd1060a0b3742e77e8c040a61
Anshul Jain <anshuljain1985@gmail.com>
2020-01-20 16:08:43 +0530

    fix

--- src/org/traccar/protocol/Et300ProtocolDecoder.java
+++ src/org/traccar/protocol/Et300ProtocolDecoder.java
@@ -183,7 +183,8 @@ public class Et300ProtocolDecoder extends BaseProtocolDecoder {
             response.writeShort(index);
             response.writeShort(Checksum.crc16(Checksum.CRC16_X25,
                     response.nioBuffer(2, response.writerIndex() - 2)));
-            response.writeByte('\r'); response.writeByte('\n'); // ending
+            response.writeByte('\r');
+            response.writeByte('\n'); // ending
             channel.writeAndFlush(new NetworkMessage(response, channel.remoteAddress()));
         }
     }
