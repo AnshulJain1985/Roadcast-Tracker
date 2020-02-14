9fb86516c46353b225ea32ef950723a47258bf4a
Anshul Jain <anshuljain1985@gmail.com>
2020-02-14 15:16:26 +0530

    fix

--- src/org/traccar/protocol/RdmAISProtocolDecoder.java
+++ src/org/traccar/protocol/RdmAISProtocolDecoder.java
@@ -15,7 +15,6 @@ import org.traccar.model.Network;
 import org.traccar.model.Position;
 
 import java.net.SocketAddress;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
 
@@ -61,7 +60,8 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
 
 
     private static final Pattern PATTERN = new PatternBuilder()
-            .text("$ID01")
+            .text("$")
+            .expression("([^,]+)?")                // packet header
             .expression("([^,]+)?,")                // Vendor Id
             .expression("([^,]+)?,")                // Software version
             .expression("([A-Z]+),")                // Packet Type
@@ -104,65 +104,27 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
             .compile();
 
     private static final Pattern PATTERN_HEARTBEAT = new PatternBuilder()
-            .text("$")
+            .text("$,")
+            .expression("([^,]+)?,")                // Header
             .expression("([^,]+)?,")                // Vendor Id
             .expression("([^,]+)?,")                // Software version
-            .expression("([A-Z]+),")                // Packet Type
-            .number("(d+),")                        // Alert ID
-            .expression("([HL]),")                    // Packet Status
             .expression("([0-9]+),")                // IMEI
-            .expression("([^,]+)?,")                // vehicle reg no
-            .number("(d+),")                        // GPS Fix
             .number("(dd)(dd)(dddd),")                // date utc (DDMMYYYY)
             .number("(dd)(dd)(dd),")                // time utc (hhmmss)
-            .number("(-?d+.d+),")                   // latitude
-            .expression("([NS]),")
-            .number("(-?d+.d+),")                   // longitude
-            .expression("([EW]),")
-            .number("(d+.d+),")                     // speed
-            .number("(d+.?d*)?,?")                    // course
-            .number("(d+),")                        // No of satellites
-            .number("(d+.?d*)?,?")                  // altitude
-            .number("(d+.?d*)?,?")                   // pdop
-            .number("(d+.?d*)?,?")                  // hdop
-            .expression("([^,]+)?,")                // Operator Name
-            .number("(d),")                         // Ignition
-            .number("(d),")                         // Main power status
-            .number("(d+.?d*)?,?")                  // Main input voltage
-            .number("(d+.?d*)?,?")                  // internal battery voltage
-            .number("(d),")                         // Emergency Status
-            .expression("([OCN]),").optional()                  // Temper alert
-            .number("(d+),")                        // GSM signal strength
-            .expression("([^,]+)?,")                // MCC
-            .expression("([^,]+)?,")                // MNC
-            .expression("([^,]+)?,")                // LAC
-            .expression("([^,]+)?,")                // CELLID
-            .expression("([^,]+)?,")                 // NMR1 signal strength
-            .expression("([^,]+)?,")                // NMR1 CellID
-            .expression("([^,]+)?,")                // NMR1 LAC
-            .expression("([^,]+)?,")                 // NMR2 signal strength
-            .expression("([^,]+)?,")                // NMR2 CellID
-            .expression("([^,]+)?,")                // NMR2 LAC
-            .expression("([^,]+)?,")                 // NMR3 signal strength
-            .expression("([^,]+)?,")                // NMR3 CellID
-            .expression("([^,]+)?,")                // NMR3 LAC
-            .expression("([^,]+)?,")                        // NMR4 signal strength
-            .expression("([^,]+)?,")                // NMR4 CellID
-            .expression("([^,]+)?,")                // NMR4 LAC
+            .number("(d+.?d*)?,?")                 // battery percentage
+            .number("(d+.?d*)?,?")                 // low battery threshold
+            .number("(d+.?d*)?,?")                 // memory percentage
+            .number("(d+),")                        // ignition on timer
+            .number("(d+),")                        // ignition off timer
             .number("(d)(d)(d)(d),")                // digital Input 4
             .number("(d)(d),")                      // digital Output 2
-            .number("(d+),")                        // Frame number
             .number("(d+.?d*)?,")                  // Analog input 1
-            .number("(d+.?d*)?,")                  // Analog input 2
-            .expression("[^,]*,")                   //Delta distance
-            .expression("(.*)")                   // OTA response
             .text("*")
-            .number("[x]+")                   // checksum
             .any()
             .compile();
 
     private static final Pattern PATTERN_EMERGENCY = new PatternBuilder()
-            .text("$Header,")
+            .text("$EPB,")
             .expression("([^,]+)?,")
             .expression("([A-Z]+),")                // Packet Type
             .expression("([0-9]+),")                // IMEI
@@ -180,8 +142,6 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
             .expression("([GN]),")                  // Provider
             .expression("([^,]+)?,")                // vehicle reg no
             .expression("([^,]+)?,")                // reply number
-            .expression("([^,]*)?,")                // LAC
-            .expression("([^,]*)?")                // MCC
             .text("*")
             .number("[x]+")                   // checksum
             .compile();
@@ -274,7 +234,7 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
         position.set(Position.KEY_VERSION_FW, parser.next());
         position.set(Position.KEY_VERSION_HW, parser.next());
         position.setDeviceTime(parser.nextDateTime());
-        String gpsFix = parser.next();
+        position.setValid(parser.nextInt(0) == 1);
         position.setLatitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_HEM));
         position.setLongitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_HEM));
         position.setAltitude(parser.nextDouble());
@@ -283,8 +243,8 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
     }
 
     private Object decodeHeartbeat(Position position, Channel channel, SocketAddress remoteAddress, Parser parser) {
-        String techName = parser.next();
-        position.set(Position.KEY_VERSION_HW, parser.next());
+        String header = parser.next();
+        String vendorId = parser.next();
         position.set(Position.KEY_VERSION_FW, parser.next());
         String imei = parser.next();
         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
@@ -292,25 +252,28 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
             return null;
         }
         position.setDeviceId(deviceSession.getDeviceId());
+        getLastLocation(position, null);
+
+        position.setDeviceTime(parser.nextDateTime());
+
         position.set(Position.KEY_BATTERY_LEVEL, parser.nextDouble(0));
 
-        getLastLocation(position, null);
 
         double lowBatThreshold = parser.nextDouble();
         double memPercentage = parser.nextDouble();
-        double memPercentage2 = parser.nextDouble();
         int intervalIgnitionOn = parser.nextInt();
         int intervalIngitionOff = parser.nextInt();
 
         for (int i = 1; i <= 4; i++) {
             int tempDio = parser.nextInt(0);
             position.set(Position.PREFIX_IN + i, tempDio);
-            if (i == 2) {
-                position.set(Position.KEY_DOOR, tempDio == 1);
-            }
         }
-        position.set(Position.PREFIX_ADC + 1, parser.nextDouble(0));
-        position.set(Position.PREFIX_ADC + 2, parser.nextDouble(0));
+
+        for (int i = 1; i <= 2; i++) {
+            position.set(Position.PREFIX_OUT + i, parser.nextInt(0));
+        }
+
+        position.set(Position.PREFIX_ADC, parser.nextDouble(0));
         return position;
     }
 
@@ -346,19 +309,13 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
         String deviceName = parser.next();
         String replyNumber = parser.next();
 
-        Network network = new Network();
-        int lac = parser.nextHexInt();
-        int cellId = parser.nextHexInt();
-
-        network.addCellTower(CellTower.fromLacCid(lac, cellId));
-
 //        String checksum = parser.next();
         return position;
     }
 
     private Object decodeNormal(Position position, Channel channel, SocketAddress remoteAddress, Parser parser) {
         String header = parser.next();
-//        position.set(Position.KEY_VERSION_HW, parser.next());
+        position.set(Position.KEY_VERSION_HW, parser.next());
         position.set(Position.KEY_VERSION_FW, parser.next());
         String packetType = parser.next();
         int alertId = parser.nextInt(0);
@@ -392,7 +349,6 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
         position.set(Position.KEY_BATTERY, parser.nextDouble(0));
         position.set(Position.KEY_STATUS, parser.nextInt(0));
 
-        String temperAlert = parser.next();
         position.set(Position.KEY_RSSI, parser.nextInt(0));
         Network network = new Network();
         int mcc = parser.nextHexInt();
@@ -432,14 +388,14 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
 
         Parser parser = new Parser(PATTERN_LOGIN, sentence);
         if (parser.matches()) {
-            String currentDate = new SimpleDateFormat("ddMMyyyyHHmmss").format(new Date());
-            channel.write("$LP" + currentDate + "*");
+//            String currentDate = new SimpleDateFormat("ddMMyyyyHHmmss").format(new Date());
+//            channel.write("$LP" + currentDate + "*");
             return decodeLogin(position, channel, remoteAddress, parser);
         }
 
         parser = new Parser(PATTERN_HEARTBEAT, sentence);
         if (parser.matches()) {
-            channel.write("$HBT*");
+//            channel.write("$HBT*");
             return decodeHeartbeat(position, channel, remoteAddress, parser);
         }
 
@@ -448,9 +404,6 @@ public class RdmAISProtocolDecoder extends BaseProtocolDecoder
             return decodeEmergency(position, channel, remoteAddress, parser);
         }
 
-        \$ID01,([^,]+)?,([A-Z]+),([0-9]+),([A-Z]+),(\d{2})(\d{2})(\d{4})(\d{2})(\d{2})(\d{2}),([VA]),(-?\d+\.\d+),([NS]),(-?\d+\.\d+),([EW]),(\d+\.?\d*)?,?(\d+\.\d+),(\d+\.\d+),([GN]),([^,]+)?,([^,]+)?,([^,]*)?,([^,]*)?\*[[0-9a-fA-F]]+
-
-        $ID01,RDME,V1.0.8,NR,1,L,864376047795371,DL3CBZ1122,1,27092019,102708,28.651234,N,77.098751,E,0.0,0.0,10,221.0,2.3,1.0,IDEA,0,1,11.\n8,4.0,0,25,404,04,00F1,6769,6767,00F1,40,7244,00F1,37,71AF,00F1,30,6E8E,0385,29,0000,00,0.00,0.00,000014,2A,*
         parser = new Parser(PATTERN, sentence);
         if (parser.matches()) {
             return decodeNormal(position, channel, remoteAddress, parser);
