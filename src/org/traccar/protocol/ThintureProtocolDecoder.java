eb1d9ad064463e346f3d231f86ed3b34f1ede5da
Anshul Jain <anshuljain1985@gmail.com>
2021-04-03 14:08:02 +0530

    fixes

---
+++ src/org/traccar/protocol/ThintureProtocolDecoder.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2013 - 2018 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import io.netty.channel.Channel;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.Context;
+import org.traccar.DeviceSession;
+import org.traccar.Protocol;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class ThintureProtocolDecoder extends BaseProtocolDecoder {
+
+    public ThintureProtocolDecoder(Protocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("&")
+            .expression("([^,]*),")               // imei
+            .number("(dd)(dd)(dd)")               // date (ddmmyy)
+            .number("(dd)(dd)(dd),")              // time (hhmmss)
+            .expression("([AV]),")
+            .number("(dd)(dd.dddd)([NS]),")       // latitude
+            .number("(ddd)(dd.dddd)([EW]),")      // longitude
+            .number("(d+.?d*),")                  // speed
+            .number("(d+.?d*),")                  // course
+            .expression("([^,]*),")               // temperature or fuel
+            .number("(d)(d)(d)(d)(d)(d)(d)(d),")  // status bits
+            .expression("([NO])")                 // speed type
+            .number("([12])")                     // live/history
+            .any()
+            .compile();
+
+    @Override
+    protected Object decode(
+            Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        Parser parser = new Parser(PATTERN, (String) msg);
+        if (!parser.matches()) {
+            return null;
+        }
+
+        Position position = new Position(getProtocolName());
+
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, parser.next());
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+
+        position.setTime(parser.nextDateTime(Parser.DateTimeFormat.DMY_HMS));
+
+        position.setValid(parser.next().equals("A"));
+        position.setLatitude(parser.nextCoordinate());
+        position.setLongitude(parser.nextCoordinate());
+        position.setSpeed(parser.nextDouble(0));
+        position.setCourse(parser.nextDouble(0));
+
+        parser.next();
+
+        parser.nextInt();
+        position.set(Position.KEY_IGNITION, parser.nextInt(0) == 1);
+        parser.nextInt();
+        position.set(Position.KEY_DOOR, parser.nextInt(0) == 1);
+        position.set(Position.ALARM_SOS, parser.nextInt(0) == 1);
+        boolean isCharge = parser.nextInt(0) == 1;
+        position.set(Position.KEY_CHARGE, isCharge);
+        parser.nextInt();
+        parser.nextInt();
+
+        Position last = Context.getIdentityManager().getLastPosition(position.getDeviceId());
+        if (last != null) {
+            if (last.getAttributes().containsKey(Position.KEY_CHARGE)
+                    && (position.getBoolean(Position.KEY_CHARGE) != isCharge)) {
+                position.set(Position.KEY_ALARM, Position.ALARM_POWER_CUT);
+            }
+        }
+
+        parser.next();
+        parser.next();
+        return position;
+    }
+
+}
