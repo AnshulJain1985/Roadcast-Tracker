cbe89350bbfe5169dd397b564177fb348a1560d7
Anshul Jain <anshuljain1985@gmail.com>
2021-06-03 17:55:59 +0530

    gradle build

---
+++ src/main/java/org/traccar/events/GeofenceEventHandler.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.events;
+
+import io.netty.channel.ChannelHandler;
+import org.traccar.BaseEventHandler;
+import org.traccar.Context;
+import org.traccar.database.GeofenceManager;
+import org.traccar.model.Calendar;
+import org.traccar.model.Device;
+import org.traccar.model.Event;
+import org.traccar.model.Position;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+@ChannelHandler.Sharable
+public class GeofenceEventHandler extends BaseEventHandler {
+
+    private final GeofenceManager geofenceManager;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeofenceEventHandler.class);
+
+    public GeofenceEventHandler() {
+        geofenceManager = Context.getGeofenceManager();
+    }
+
+    @Override
+    protected Map<Event, Position> analyzePosition(Position position) {
+        Device device = Context.getIdentityManager().getById(position.getDeviceId());
+        if (device == null) {
+            return null;
+        }
+        if (!Context.getIdentityManager().isLatestPosition(position) || !position.getValid()) {
+            return null;
+        }
+
+        List<Long> currentGeofences = geofenceManager.getCurrentDeviceGeofences(position);
+        List<Long> oldGeofences = new ArrayList<>();
+        if (device.getGeofenceIds() != null) {
+            oldGeofences.addAll(device.getGeofenceIds());
+        }
+        List<Long> newGeofences = new ArrayList<>(currentGeofences);
+
+        if (device.getId() == 13350) {
+            LOGGER.info("GeoFix position: " + position.getLatitude() + ' ' + position.getLongitude());
+            LOGGER.info("GeoFix currentGeofences: " + Arrays.toString(currentGeofences.toArray()));
+            LOGGER.info("GeoFix oldGeofences: " + Arrays.toString(oldGeofences.toArray()));
+        }
+
+        newGeofences.removeAll(oldGeofences);
+        oldGeofences.removeAll(currentGeofences);
+
+
+
+        device.setGeofenceIds(currentGeofences);
+
+        Map<Event, Position> events = new HashMap<>();
+        for (long geofenceId : oldGeofences) {
+            long calendarId = geofenceManager.getById(geofenceId).getCalendarId();
+            Calendar calendar = calendarId != 0 ? Context.getCalendarManager().getById(calendarId) : null;
+            if (calendar == null || calendar.checkMoment(position.getFixTime())) {
+                Event event = new Event(Event.TYPE_GEOFENCE_EXIT, position.getDeviceId(), position.getId());
+                event.setGeofenceId(geofenceId);
+                events.put(event, position);
+            }
+        }
+        for (long geofenceId : newGeofences) {
+            long calendarId = geofenceManager.getById(geofenceId).getCalendarId();
+            Calendar calendar = calendarId != 0 ? Context.getCalendarManager().getById(calendarId) : null;
+            if (calendar == null || calendar.checkMoment(position.getFixTime())) {
+                Event event = new Event(Event.TYPE_GEOFENCE_ENTER, position.getDeviceId(), position.getId());
+                event.setGeofenceId(geofenceId);
+                events.put(event, position);
+            }
+        }
+        return events;
+    }
+}
---
+++ src/main/java/org/traccar/protocol/GpsBoxProtocolDecoder.java
@@ -0,0 +1,227 @@
+/*
+ * Copyright 2012 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.protocol;
+
+import io.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.traccar.BaseProtocolDecoder;
+import org.traccar.Context;
+import org.traccar.DeviceSession;
+import org.traccar.NetworkMessage;
+import org.traccar.helper.DateBuilder;
+import org.traccar.helper.Parser;
+import org.traccar.helper.PatternBuilder;
+import org.traccar.model.Position;
+
+import java.net.SocketAddress;
+import java.util.regex.Pattern;
+
+public class GpsBoxProtocolDecoder extends BaseProtocolDecoder {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GpsBoxProtocolDecoder.class);
+
+    public GpsBoxProtocolDecoder(GpsBoxProtocol protocol) {
+        super(protocol);
+    }
+
+    private static final Pattern PATTERN = new PatternBuilder()
+            .text("A:")
+            .number("(d+),")                     // imei
+            .text("B:")
+            .number("(d+.d)*[Vv]*,")              // battery in volts
+            .text("C:")
+            .number("(d+.d)*[NA]*,")              // analog in volts
+            .text("D:")
+            .number("(dd)")                     // time in HH
+            .number("(dd)")                     // time in MM
+            .number("(dd),")                    // time in SS
+            .text("E:")
+            .number("(d+)(dd.d+)")              // latitude (ddmm.mmmm)
+            .expression("([NSEW])").optional()
+            .text(",F:")
+            .number("(d+)(dd.d+)")              // longitude (dddmm.mmmm)
+            .expression("([EW])?").optional()
+            .text(",G:")
+            .number("(d+.?d*)?,")                // speed
+            .text("H:")
+            .number("(dd)")                     // date in DD , GMT 0
+            .number("(dd)")                     // date in MM , GMT 0
+            .number("(dd),")                     // date in YY , GMT 0
+            .text("I:")
+            .expression("([GL]),")                 // FORMAT (G for GPS [value in Degree], L for LBS [value in seconds])
+            .text("J:")
+//            .expression("([^,]+),")            // bit1
+            .number("(x)")                       // Engine ON/OFF status bit
+            .number("(x)")                       // SOS button pressed or not status bit
+            .number("(x)")                       // Security Mode Bit
+            .number("(x)")                       // AC ON/OFF status bit
+            .number("(x)")                       // Shock status bit
+            .number("(x)")                       // External battery connected or not status bit
+            .number("(x)")                       // Tempering switch pressed or not status bit
+            .number("(x),")                      // Over speed status bit
+            .text("K:")
+//            .expression("([^,]+),")            // bit2
+            .number("(x)")                       // Immobilize Status
+            .number("(x)")                       //
+            .number("(x)")                       //
+            .number("(x)")                       //
+            .number("(x)")                       //
+            .number("(x)")                       //
+            .number("(x)")                       //
+//            .number("(.),")                      // For GPS Fix Indication (‘A’ if GPS fix and ‘V’ if GPS not fixed )
+            .expression("([A-Za-z0-9]+),")
+            .text("L:")
+            .number("(d+),")                     // pin
+            .text("M:")
+            .number("(d+.?d*)?,?")               // course
+            .any()
+            .compile();
+
+    @Override
+    protected Object decode(Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
+
+        String sentence = (String) msg;
+
+        if (!sentence.isEmpty()) {
+//            LOGGER.info("GPSBOX: " + sentence);
+
+            if (channel != null) {
+                channel.writeAndFlush(new NetworkMessage("+##Received OK", remoteAddress));
+            }
+            int start = sentence.indexOf("{");
+            if (start >= 0) {
+                sentence = sentence.substring(start + 1, sentence.length() - 1);
+            } else {
+                return null;
+            }
+        }
+
+        Position position = new Position(getProtocolName());
+
+        Parser parser = new Parser(PATTERN, sentence);
+        if (!parser.matches()) {
+            return null;
+        }
+
+        String imei = parser.next();
+        DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
+        if (deviceSession == null) {
+            return null;
+        }
+        position.setDeviceId(deviceSession.getDeviceId());
+        position.set(Position.KEY_BATTERY, parser.nextDouble(0));
+        position.set(Position.KEY_POWER, parser.nextDouble(0));
+
+        int localHours = parser.nextInt(0);
+        int localMinutes = parser.nextInt(0);
+        int localSeconds = parser.nextInt(0);
+
+        position.setLatitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_MIN_HEM));
+        position.setLongitude(parser.nextCoordinate(Parser.CoordinateFormat.DEG_MIN_HEM));
+//        position.setSpeed(parser.nextDouble(0));
+        position.setSpeed(convertSpeed(parser.nextDouble(0), "kmh"));
+
+        DateBuilder dateBuilder = new DateBuilder()
+                .setDateReverse(parser.nextInt(0), parser.nextInt(0), parser.nextInt(0));
+        dateBuilder.setTime(localHours, localMinutes, localSeconds);
+        position.setTime(dateBuilder.getDate());
+        position.setValid(parser.next().equals("G"));
+
+        position.set(Position.KEY_IGNITION, parser.nextInt(0) == 1);
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.KEY_ALARM, Position.ALARM_SOS);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.KEY_INPUT, true); //Need to check and update
+        }
+        position.set(Position.KEY_DOOR, parser.nextInt(0) == 1);
+        position.set(Position.KEY_STATUS, parser.nextInt(0));
+        if (parser.nextInt(0) == 0) {
+            Position last = Context.getIdentityManager().getLastPosition(position.getDeviceId());
+            if (last != null && last.getBoolean(Position.KEY_CHARGE)) {
+                position.set(Position.KEY_ALARM, Position.ALARM_POWER_CUT);
+            }
+            position.set(Position.KEY_CHARGE, false);
+        } else {
+            position.set(Position.KEY_CHARGE, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.KEY_ALARM, Position.ALARM_TAMPERING);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.KEY_ALARM, Position.ALARM_OVERSPEED);
+        }
+
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.KEY_COMMAND, "Immobilize");
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.ALARM_TOW, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.PREFIX_IO + 1, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.PREFIX_IO + 2, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.PREFIX_IO + 3, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.PREFIX_IO + 4, true);
+        }
+        if (parser.nextInt(0) == 1) {
+            position.set(Position.PREFIX_IO + 5, true);
+        }
+
+        Position last = Context.getIdentityManager().getLastPosition(position.getDeviceId());
+
+//        boolean speedLogic = (position.getSpeed() > 0.0
+//                && last.getSpeed() == 0.0)
+//                || (position.getSpeed() == 0.0 && last.getSpeed() > 0.0)
+//                || position.getSpeed() > 0.0;
+
+//        boolean speedLogic = last == null || (position.getSpeed() > 0.0
+//                && last.getSpeed() > 0.0);
+
+//        if (last == null || (parser.next().equals("A") && (
+//                position.getBoolean(Position.KEY_IGNITION)
+//                || (last.getLatitude() != position.getLatitude()
+//                && last.getLongitude() != position.getLongitude()
+//                && speedLogic)
+//                || (last.getCourse() != position.getCourse()
+//                && speedLogic)))) {
+
+        String gpsStatus = parser.next();
+//        position.setSpeed(position.getSpeed());
+
+//        Log.debug("GPS: " + gpsStatus + "  Lat: " + position.getLatitude() + "  Lng: "
+//                + position.getLongitude() + " Speed: " + position.getSpeed());
+
+        if (last == null || (gpsStatus.equals("A"))) {
+            position.set(Position.KEY_GPS, true);
+            position.set(Position.KEY_VIN, parser.next());
+            position.setCourse(parser.nextDouble(0));
+        } else {
+            getLastLocation(position, null);
+        }
+
+
+        return position;
+    }
+
+}
